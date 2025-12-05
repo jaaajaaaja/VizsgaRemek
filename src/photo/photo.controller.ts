78a77608fd0a5646ba9ecf0a88438daedd894273
jaaajaaaja <andkob0126@gmail.com>
2025-12-05 08:36:33 +0100

    photo get endpoints now return the data correctly with the correct headers

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors, Response } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 import type { Express } from 'express';
 import { PhotoService } from './photo.service';
 import { FileValidationPipe } from 'src/common/validators/file-validation.pipe';
 import { UpdatePhotoDto } from './dto/update-photo.dto';
+import type { Response as Res } from 'express';
 
 @Controller('photo')
 export class PhotoController {
@@ -16,17 +17,20 @@ export class PhotoController {
     }
 
     @Get(':id')
-    async getOne(@Param('id') id:string) {
-        return this.photoService.getOne(Number(id))
+    async getOne(@Param('id') id: string, @Response() response: Res) {
+        const res = await this.photoService.getOne(Number(id))
+        return response.set({ "Content-Type": res?.type }).send(res?.data);
     }
 
     @Delete(':id')
-    async remove(@Param('id') id:string) {
+    async remove(@Param('id') id: string) {
         return this.photoService.remove(Number(id))
     }
 
     @Put(':id')
-    async update(@Param('id') id:string, @Body() data:UpdatePhotoDto) {
+    async update(@Param('id') id: string, @Body() data: UpdatePhotoDto) {
+        this.photoService.increaseMaxAllowedPacket();
+
         return this.photoService.update(Number(id), data)
     }
 
@@ -34,6 +38,9 @@ export class PhotoController {
     @UseInterceptors(FileInterceptor('file', { storage: memoryStorage() }))
     async uploadFile(@UploadedFile(new FileValidationPipe()) file: Express.Multer.File,
         @Body('userID') userID: string, @Body('placeID') placeID: string) {
+
+        this.photoService.increaseMaxAllowedPacket();
+
         return this.photoService.add({
             data: file.buffer,             //new Uint8Array()
             type: file.mimetype,
--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -5,19 +5,19 @@ import { UpdatePhotoDto } from './dto/update-photo.dto';
 
 @Injectable()
 export class PhotoService {
-    constructor(private prisma:PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
     async getAll() {
         return this.prisma.photo.findMany()
     }
 
-    async getOne(id:number) {
-        return this.prisma.photo.findUnique({ where: {id} })
+    async getOne(id: number) {
+        return this.prisma.photo.findUnique({ where: { id } })
     }
 
-    async add(data:CreatePhotoDto) { 
+    async add(data: CreatePhotoDto) {
         // return this.prisma.photo.create({ data })
-        
+
         const buffer = Buffer.isBuffer(data.data) ? data.data : Buffer.from(data.data as any)
         const createPayload = {
             data: buffer,
@@ -29,11 +29,11 @@ export class PhotoService {
         return this.prisma.photo.create({ data: createPayload })
     }
 
-    async remove(id:number) {
-        return this.prisma.photo.delete({ where: {id} })
+    async remove(id: number) {
+        return this.prisma.photo.delete({ where: { id } })
     }
 
-    async update(id:number, data:UpdatePhotoDto) {
+    async update(id: number, data: UpdatePhotoDto) {
         //return this.prisma.photo.update({ where: {id}, data })
 
         const buffer = Buffer.isBuffer(data.data) ? data.data : Buffer.from(data.data as any)
@@ -44,6 +44,16 @@ export class PhotoService {
             placeID: data.placeID,
         }
 
-        return this.prisma.photo.update({ where: {id}, data: createPayload })
+        return this.prisma.photo.update({ where: { id }, data: createPayload })
+    }
+
+    async increaseMaxAllowedPacket() {
+        const size = 10 * 1024 * 1024; // 10 MB
+
+        await this.prisma.$executeRawUnsafe(
+            `SET GLOBAL max_allowed_packet = ${size};`
+        );
+
+        return 'max_allowed_packet updated';
     }
 }
